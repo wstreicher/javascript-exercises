fdb72e7f9cf8388e2fb7eddbcde8927af938d8e7
Ward Streicher <8474691+wstreicher@users.noreply.github.com>
2022-11-13 00:50:09 -0500

    Complete first 6 challenges

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    // for (let i = 1; i <= (arguments.length - 1); i++) {
+    //     for (let a = 0; a <= array.length; a++) {
+    //         if (array.indexOf(arguments[i] != undefined)) {
+    //             array.splice(array.indexOf(arguments[i]))
+    //         }
+    //     }
+    // }
 
+    const newArray = [];
+
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }
+    });
+
+    return newArray
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if ((num1 < 0) || (num2 < 0)) {
+        return "ERROR"
+    } else if (((typeof num1 == "number") && (typeof num2 == "number"))) {
+        let sum;
+        if (num1 >= num2) {
+            sum = num2;
+            for (let i = (num2 + 1); i <= num1; i++) {
+                sum += i
+            }
+        } else {
+            sum = num1;
+            for (let i = (num1 + 1); i <= num2; i++) {
+                sum += i
+            } 
+        }
 
-};
+        return sum;
+} else {
+    return "ERROR"
+}
+}
+
 
 // Do not edit below this line
 module.exports = sumAll;
